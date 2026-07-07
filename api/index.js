9821c3ed76a8410e9d752cf7527629eeb8024299
manikanta rajesh <venkatrajeshnaidu@gmail.com>
2026-07-07 14:38:37 +0530

    Update output directory to build to bypass strict Vercel settings

--- api/index.js
+++ api/index.js
@@ -17,9 +17,9 @@ app.post('/api/contact', contactHandler);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../build')));
   app.get(/^.*$/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
   });
 }
 

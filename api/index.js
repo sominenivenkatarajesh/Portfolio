c7e5a0c4a044718065ec9b28f4fe848d759fb75a
manikanta rajesh <venkatrajeshnaidu@gmail.com>
2026-07-07 14:29:27 +0530

    Fix Express 5 wildcard routing syntax for Render deployment

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.post('/api/contact', contactHandler);
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
+  app.get(/^.*$/, (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
